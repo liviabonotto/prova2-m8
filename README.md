64d76c117b7671cba50f840161b017738878881b
Lívia Bonotto <liviabonotto@gmail.com>
2023-12-15 15:32:59 -0300

    subindo resolução

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
 # 20232BSET03P2
 Inteli - Engenharia de Software | Avaliação 2023-2B P2
+
+
+# Prova Prática
+
+Lívia Mattoso Bonotto Cabral
+
+### ENDPOINTS CATS
+
+Registrando Gato:
+
+![Untitled](./assets/registrando_gatos.png)
+
+Inserindo um nome de gato inválido:
+
+![Untitled](./assets/gato_invalido_1.png)
+
+![Untitled](./assets/gato_invalido_2.png)
+
+Gatos registrados:
+
+![Untitled](./assets/gatos_registrados.png)
+
+Votando em um gato:
+
+![Untitled](./assets/votando_gato.png)
+
+Votando em um gato não registrado: 
+
+![Untitled](./assets/votando_gato_nao_registrado.png)
+
+Pegando registro de cats e votos:
+
+![Untitled](./assets/registro_cats_votos.png)
+
+### ENDPOINTS DOGS
+
+Registrando Cachorro:
+
+![Untitled](./assets/registro_dogs_voto.png)
+
+Inserindo um nome de cachorro inválido:
+
+![Untitled](./assets/dog_invalido_1.png)
+
+![Untitled](./assets/dog_invalido_2.png)
+
+Cachorros registrados: 
+
+![Untitled](./assets/dogs_registrados.png)
+
+Votando em um cachorro:
+
+![Untitled](./assets/votando_dog.png)
+
+Votando em um cachorro não registrado: 
+
+![Untitled](/assets/votando_dog_nao_registrado.png)
+
+Pegando registro de dogs e votos:
+
+![Untitled](./assets/registro_dogs_voto.png)
+
+### Votando em outro animal (inválido):
+
+![Untitled](./assets/animal_invalido.png)
+
+### Vulnerabilidades e medidas adotadas
+
+- **SQL Injection**: resolvido usando prepared statements;
+- **Validação de entrada**: adicionada validação para tipos de dados e verificação de campos obrigatórios;
+- **Verificação de existência de registro**: implementada a verificação antes de adicionar votos;
+- **Tratamento de erros**: verificado para evitar vazamento de informações.
--- index.js
+++ index.js
@@ -1,42 +1,95 @@
-const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
-const bodyParser = require('body-parser');
+const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 
-
-const db = new sqlite3.Database(':memory:');
+const db = new sqlite3.Database(":memory:");
 
 db.serialize(() => {
-  db.run("CREATE TABLE cats (id INT, name TEXT, votes INT)");
-  db.run("CREATE TABLE dogs (id INT, name TEXT, votes INT)");
+  db.run(
+    "CREATE TABLE cats (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, votes INT)"
+  );
+  db.run(
+    "CREATE TABLE dogs (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, votes INT)"
+  );
+  //INTEGER PRIMARY KEY AUTOINCREMENT pra não precisar passar o id no body, passa na url da requisicao
 });
 
-app.post('/cats', (req, res) => {
+
+app.post("/cats", (req, res) => {
+  //aqui estou usando prepared statement para parâmetros vinculados ao invés de concatenação de strings
+
   const name = req.body.name;
-  db.run(`INSERT INTO cats (name, votes) VALUES ('${name}', 0)`, function(err) {
+
+  //permitindo apenas string e não pode ser uma string vazia, null, NaN, undefined, false ou 0 e campos obrigatórios
+  if (typeof name !== "string" || !name.length) {
+    return res.status(400).send("Nome inválido.");
+  }
+  const stmt = db.prepare("INSERT INTO cats (name, votes) VALUES (?, 0)");
+  stmt.run(name, function (err) {
     if (err) {
       res.status(500).send("Erro ao inserir no banco de dados");
     } else {
       res.status(201).json({ id: this.lastID, name, votes: 0 });
     }
   });
+  stmt.finalize();
 });
 
-app.post('/dogs', (req, res) => {
-  
+app.post("/dogs", (req, res) => {
+  //aqui estou usando prepared statement para parâmetros vinculados ao invés de concatenação de strings
+
+  const name = req.body.name;
+
+  //permitindo apenas string e não pode ser uma string vazia, null, NaN, undefined, false ou 0 e campos obrigatórios
+  if (typeof name !== "string" || !name.length) {
+    return res.status(400).send("Nome inválido.");
+  }
+  const stmt = db.prepare("INSERT INTO dogs (name, votes) VALUES (?, 0)");
+  stmt.run(name, function (err) {
+    if (err) {
+      res.status(500).send("Erro ao inserir no banco de dados");
+    } else {
+      res.status(201).json({ id: this.lastID, name, votes: 0 });
+    }
+  });
+  stmt.finalize();
 });
 
-app.post('/vote/:animalType/:id', (req, res) => {
- 
-  db.run(`UPDATE ${animalType} SET votes = votes + 1 WHERE id = ${id}`);
-  res.status(200).send("Voto computado");
+app.post("/vote/:animalType/:id", (req, res) => {
+  const { animalType, id } = req.params; //para passar o tipo e o id do animal
+
+  //validação para garantir que seja apenas cats e dogs
+  if (!["cats", "dogs"].includes(animalType)) {
+    return res.status(400).send("Animal inválido. Apenas cats e dogs.");
+  }
+  db.get(`SELECT id FROM ${animalType} WHERE id = ?`, [id], (err, row) => {
+    if (err) {
+      return res.status(500).send("Erro ao consultar o banco de dados.");
+    }
+    //validação para verificar se o animal existe no banco ou não
+    if (!row) {
+      return res.status(404).send("Registro de animal não encontrado.");
+    }
+    db.run(
+      //atualização dos fotos
+      `UPDATE ${animalType} SET votes = votes + 1 WHERE id = ?`,
+      [id],
+      (updateErr) => {
+        if (updateErr) {
+          return res.status(500).send("Erro ao atualizar o banco de dados.");
+        }
+        res.status(200).send("Voto computado!");
+      }
+    );
+  });
 });
 
-app.get('/cats', (req, res) => {
+app.get("/cats", (req, res) => {
   db.all("SELECT * FROM cats", [], (err, rows) => {
     if (err) {
       res.status(500).send("Erro ao consultar o banco de dados");
@@ -46,13 +99,19 @@ app.get('/cats', (req, res) => {
   });
 });
 
-app.get('/dogs', (req, res) => {
-  
+app.get("/dogs", (req, res) => {
+  db.all("SELECT * FROM dogs", [], (err, rows) => {
+    if (err) {
+      res.status(500).send("Erro ao consultar o banco de dados");
+    } else {
+      res.json(rows);
+    }
+  });
 });
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Ocorreu um erro!');
+  res.status(500).send("Ocorreu um erro!");
 });
 
 app.listen(port, () => {
